90f5dd695dfe498d0e7283a63f0bf6b33edcefee
bright <obololastkiller@gmail.com>
2023-11-22 22:38:16 +0000

    At method added

--- Arrays/Simple_Array_Methods.js
+++ Arrays/Simple_Array_Methods.js
@@ -4,12 +4,12 @@
  */
 
 let arr = ["a", "b", "c", "d", "e", "f"];
-console.log(arr.slice(3));
+// console.log(arr.slice(3));
 // Outcome: ['d', 'e', 'f']
 
 /********* Finding the last element of the array */
 
-console.log(arr.slice(-2));
+// console.log(arr.slice(-2));
 // Outcome: ['e', 'f']
 
 /***********************************
@@ -19,9 +19,9 @@ console.log(arr.slice(-2));
  * *********************************
  */
 
-console.log(arr.splice(2));
+// console.log(arr.splice(2));
 // Outcome: ['c', 'd', 'e', 'f']
-console.log(arr);
+// console.log(arr);
 // Outcome: ['a', 'b']  The original array has been altered
 
 /***********************************
@@ -33,7 +33,7 @@ console.log(arr);
  */
 
 let arr2 = ["j", "i", "h", "g", "f"];
-console.log(arr2.reverse());
+// console.log(arr2.reverse());
 // Outcome: ['f', 'g', 'h', 'i', 'j']
 
 /***********************************
@@ -44,7 +44,7 @@ console.log(arr2.reverse());
  */
 arr = ["a", "b", "c", "d", "e"];
 const letters = arr.concat(arr2);
-console.log(letters);
+// console.log(letters);
 // Outcome: ['a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j']
 
 /***********************************
@@ -54,5 +54,22 @@ console.log(letters);
  * *********************************
  */
 
-console.log(letters.join("-"));
+// console.log(letters.join("-"));
 //Outcome: a-b-c-d-e-f-g-h-i-j
+
+// console.log(letters.join(","));
+
+/***********************************
+ * AT Method
+ * This method gets a value from
+ * the exact position of the  array
+ * *********************************
+ */
+
+const arr3 = [23, 11, 64];
+console.log(arr3.at(0));
+// Outcome: 23
+
+// Getting the last element using the At method
+console.log(arr3.at(-1));
+//Outcome: 64
