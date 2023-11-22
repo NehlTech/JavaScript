d3c060f8f49177bd03917afdd312c61790d9bf14
bright <obololastkiller@gmail.com>
2023-11-22 23:47:26 +0000

    ForEach Method Updated

--- Arrays/Looping_Arrays_forEach.js
+++ Arrays/Looping_Arrays_forEach.js
@@ -5,12 +5,14 @@
  * over an array
  * It requires a callback function
  * to tell it what to do
+ * ForEach method loops through the
+ * entire array
  * *********************************
  */
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]; // the +ve's are deposits and -ve's are witthdrawals
 
-//Using the for loop
+//Using the for-of loop
 // for (const movement of movements) {
 //   if (movement > 0) {
 //     console.log(`You deposited ${movement}`);
@@ -21,10 +23,21 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]; // the +ve's are
 
 //Using forEach method
 console.log("*********** FOR EACH METHOD **************");
-movements.forEach(function (transaction) {
-  if (transaction > 0) {
-    console.log(`You deposited ${transaction}`);
+// movements.forEach(function (transaction) {
+//   if (transaction > 0) {
+//     console.log(`You deposited ${transaction}`);
+//   } else {
+//     console.log(`You withdrew ${Math.abs(transaction)}`);
+//   }
+// });
+
+//Getting the currrent index for forEach
+movements.forEach(function (trans, currIndex, arr) {
+  if (trans > 0) {
+    console.log(`Transaction ${currIndex + 1}: You deposited ${trans}`);
   } else {
-    console.log(`You withdrew ${Math.abs(transaction)}`);
+    console.log(
+      `Transaction ${currIndex + 1}: You withdrew ${Math.abs(trans)}`
+    );
   }
 });
