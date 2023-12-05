a136abc2be0883820d0c4e2861a1cb69ae8ee7cc
bright <obololastkiller@gmail.com>
2023-12-05 00:42:10 +0000

    Array Methods Update

--- Arrays/BankistApp/bankistScript.js
+++ Arrays/BankistApp/bankistScript.js
@@ -39,54 +39,65 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
-//Elements
-const labelWelcome = document.querySelector(".welcome");
-const labelDate = document.querySelector(".date");
-const labelBalance = document.querySelector(".balance__value");
-const labelSumIn = document.querySelector(".summary__value--in");
-const labelSumOut = document.querySelector(".summary__value--out");
-const labelSumInterest = document.querySelector(".summary__value--interest");
-const labelTimer = document.querySelector(".timer");
+// //Elements
+// const labelWelcome = document.querySelector(".welcome");
+// const labelDate = document.querySelector(".date");
+// const labelBalance = document.querySelector(".balance__value");
+// const labelSumIn = document.querySelector(".summary__value--in");
+// const labelSumOut = document.querySelector(".summary__value--out");
+// const labelSumInterest = document.querySelector(".summary__value--interest");
+// const labelTimer = document.querySelector(".timer");
 
-const containerApp = document.querySelector(".app");
-const containerMovements = document.querySelector(".movements");
+// const containerApp = document.querySelector(".app");
+// const containerMovements = document.querySelector(".movements");
 
-const btnLogin = document.querySelector(".login__btn");
-const btnTransfer = document.querySelector(".form__btn--transfer");
-const btnLoan = document.querySelector(".form__btn--loan");
-const btnClose = document.querySelector(".form__btn--close");
-const btnSort = document.querySelector(".btn--sort");
+// const btnLogin = document.querySelector(".login__btn");
+// const btnTransfer = document.querySelector(".form__btn--transfer");
+// const btnLoan = document.querySelector(".form__btn--loan");
+// const btnClose = document.querySelector(".form__btn--close");
+// const btnSort = document.querySelector(".btn--sort");
 
-const inputLoginUsername = document.querySelector(".login__input--user");
-const inputLoginPin = document.querySelector(".login__input--pin");
-const inputTransferTo = document.querySelector(".form__input--to");
-const inputTransferAmount = document.querySelector(".form__input--amount");
-const inputLoanAmount = document.querySelector(".form__input--loan-amount");
-const inputCloseUsername = document.querySelector(".form__input--user");
-const inputClosePin = document.querySelector(".form__input--pin");
+// const inputLoginUsername = document.querySelector(".login__input--user");
+// const inputLoginPin = document.querySelector(".login__input--pin");
+// const inputTransferTo = document.querySelector(".form__input--to");
+// const inputTransferAmount = document.querySelector(".form__input--amount");
+// const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+// const inputCloseUsername = document.querySelector(".form__input--user");
+// const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayTransactions = function (transactions) {
-  containerMovements.innerHTML = "";
+// const displayTransactions = function (transactions) {
+//   containerMovements.innerHTML = "";
 
-  transactions.forEach(function (transfer, i) {
-    const type = transfer > 0 ? "deposit" : "withdrawal";
-    const html = `
-    <div class="movements__row">
-      <div class="movements__type movements__type--${type}">
-        ${i + 1}
-        ${type}
-      </div>
-      <div class="movements__value">
-       ${transfer + "$"}
-      </div>   
-    
-    </div>
-    `;
-    containerMovements.insertAdjacentHTML("afterbegin", html);
-  });
-};
+//   transactions.forEach(function (transfer, i) {
+//     const type = transfer > 0 ? "deposit" : "withdrawal";
+//     const html = `
+//     <div class="movements__row">
+//       <div class="movements__type movements__type--${type}">
+//         ${i + 1}
+//         ${type}
+//       </div>
+//       <div class="movements__value">
+//        ${transfer + "$"}
+//       </div>
 
-displayTransactions(account1.transactions);
+//     </div>
+//     `;
+//     containerMovements.insertAdjacentHTML("afterbegin", html);
+//   });
+// };
+
+// displayTransactions(account1.transactions);
+
+/**Computing Username */
+
+// const user = "Steven Thomas Williams"; // stw
+// const userName = user
+//   .toLowerCase()
+//   .split(" ")
+//   .map((name) => name[0])
+//   .join("");
+
+// console.log(userName);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -99,3 +110,30 @@ const currencies = new Map([
 ]);
 
 const transactions = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//Creating functions to receive any argument
+
+// A function named createUsernames is declared, which takes
+// an array of accounts (accs) as a parameter. Inside the function:
+
+// The forEach method is used to iterate over each account in the accs array.
+// For each account, a new property username is added to the account object.
+// The username is created by taking the owner property, converting
+//  it to lowercase, splitting it into an array of words, mapping
+// each word to its first character, and finally joining these characters into a single string.
+
+// const createUsernames = function (accs) {
+//   accs.forEach(function (acc) {
+//     acc.username = acc.owner
+//       .toLowerCase()
+//       .split(" ")
+//       .map((word) => word[0])
+//       .join("");
+//   });
+// };
+
+// createUsernames(accounts);
+// console.log(accounts);
+
+const withdrawals = transactions.filter((mov) => mov < 0);
+console.log(withdrawals);
