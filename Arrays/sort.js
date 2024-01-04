770e4216f16d906d9b6ae859f38803fcb9e8173a
bright <obololastkiller@gmail.com>
2024-01-04 15:59:01 +0000

    Sort updated

--- Arrays/sort.js
+++ Arrays/sort.js
@@ -36,19 +36,56 @@
  *
  */
 
-const products = [
-  {
-    name: "laptop",
-    price: 1000,
-  },
-  {
-    name: "desktop",
-    price: 1500,
-  },
-  {
-    name: "phone",
-    price: 500,
-  },
-];
+// const products = [
+//   {
+//     name: "laptop",
+//     price: 1000,
+//   },
+//   {
+//     name: "desktop",
+//     price: 1500,
+//   },
+//   {
+//     name: "phone",
+//     price: 500,
+//   },
+// ];
 
-console.log(products.sort((a, b) => a.price - b.price));
+// console.log(products.sort((a, b) => a.price - b.price));
+
+const account1 = {
+  owner: "Kwaku Manu",
+  transactions: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  transactions: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5, // %
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  transactions: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7, // %
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  transactions: [430, 1000, 700, 50, 90],
+  interestRate: 1, // %
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+let trans = accounts.sort((a, b) => {
+  if (a > b) return 1;
+  if (b > a) return -1;
+});
+
+console.log(trans);
