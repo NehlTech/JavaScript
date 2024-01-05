79cac37b41a9fd783c86b3eabddf83894f83b4d5
bright <obololastkiller@gmail.com>
2024-01-05 22:55:28 +0000

    Numeric Separators

---
+++ Numbers/numericSeparators.js
@@ -0,0 +1,18 @@
+/**
+ * Numeric Separators
+ * The numeric separators helps
+ * to read large numbers using (_)
+ */
+
+// 287,460,000,000
+const diameter = 287_460_000_000; //Using thousands separator (_)
+console.log(diameter);
+
+const price = 345_99;
+console.log(price); // Using cent separator (_)
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
