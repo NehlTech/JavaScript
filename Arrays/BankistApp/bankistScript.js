d5d06d838f402b55c7de83ca30c57dd86731ba88
bright <obololastkiller@gmail.com>
2024-01-05 19:25:55 +0000

    Bankist Updated

--- Arrays/BankistApp/bankistScript.js
+++ Arrays/BankistApp/bankistScript.js
@@ -11,10 +11,23 @@
 /*************    Data    **************** */
 
 const account1 = {
-  owner: "Kwaku Manu",
-  transactions: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  owner: "Jonas Schmedtmann",
+  transactions: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    "2019-11-18T21:31:17.178Z",
+    "2019-12-23T07:42:02.383Z",
+    "2020-01-28T09:15:04.904Z",
+    "2020-04-01T10:17:24.185Z",
+    "2020-05-08T14:11:59.604Z",
+    "2020-05-27T17:01:17.194Z",
+    "2020-07-11T23:36:17.929Z",
+    "2020-07-12T10:51:36.790Z",
+  ],
+  currency: "EUR",
+  locale: "pt-PT", // de-DE
 };
 
 const account2 = {
@@ -22,6 +35,18 @@ const account2 = {
   transactions: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5, // %
   pin: 2222,
+  movementsDates: [
+    "2019-11-01T13:15:33.035Z",
+    "2019-11-30T09:48:16.867Z",
+    "2019-12-25T06:04:23.907Z",
+    "2020-01-25T14:18:46.235Z",
+    "2020-02-05T16:33:06.386Z",
+    "2020-04-10T14:43:26.374Z",
+    "2020-06-25T18:49:59.371Z",
+    "2020-07-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
 };
 
 const account3 = {
@@ -82,7 +107,7 @@ const displayTransactions = function (transactions, sort = false) {
         ${type}
       </div>
       <div class="movements__value">
-       ${transfer + "€"}
+       ${transfer.toFixed(2) + "€"}
       </div>
 
     </div>
@@ -97,7 +122,7 @@ const calDisplayBalance = function (acc) {
   // const balance = acc.transactions.reduce((acc, trans) => acc + trans, 0);
   acc.balance = acc.transactions.reduce((acc, trans) => acc + trans, 0);
   // acc.balance = balance
-  labelBalance.textContent = `${acc.balance} €`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)} €`;
 };
 // calDisplayBalance(account1.transactions);
 
@@ -106,13 +131,13 @@ const calcDisplaySummary = function (acc) {
   const incomes = acc.transactions
     .filter((trans) => trans > 0)
     .reduce((acc, trans) => acc + trans, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   //withdrawals
   const out = acc.transactions
     .filter((trans) => trans < 0)
     .reduce((acc, trans) => acc + trans, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   //interest
   const interest = acc.transactions
@@ -120,7 +145,7 @@ const calcDisplaySummary = function (acc) {
     .map((depo) => depo * acc.interestRate)
     .filter((interest) => interest >= 1) //excluding interest less than 1
     .reduce((acc, interest) => acc + interest, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 // calcDisplaySummary(account1.transactions);
@@ -240,7 +265,7 @@ btnTransfer.addEventListener("click", function (e) {
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
   if (
     amount > 0 &&
     currentAccount.transactions.some((trans) => trans >= amount * 0.1)
