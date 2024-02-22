a86ef1ebe9d1eda9dcb2f7345dac21a0a8273bf8
bright <obololastkiller@gmail.com>
2024-02-22 03:15:57 +0000

    Lazy loading updated

--- Bankist/script.js
+++ Bankist/script.js
@@ -14,6 +14,13 @@ const section1 = document.querySelector('#section--1');
 
 const header = document.querySelector('.header'); // Selecting a class header
 
+// Tabbed Component selection
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
+
+/************* Modal window starts ************/
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -35,29 +42,31 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+/************* Modal window ends ************/
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.innerHTML =
-  "We use cookies for improved functionality and analytics. <button class= 'btn btn-close-cookie'>Got it!</button> ";
-header.append(message); // append inserts or append the element as the last child
+/************* Cookie message starts ************/
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML =
+//   "We use cookies for improved functionality and analytics. <button class= 'btn btn-close-cookie'>Got it!</button> ";
+// header.append(message); // append inserts or append the element as the last child
 
-// Deleting elements
-document
-  .querySelector('.btn-close-cookie')
-  .addEventListener('click', function () {
-    message.remove();
-  });
+// // Deleting elements
+// document
+//   .querySelector('.btn-close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
+// // Setting background color and width of the cookie message
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+
+// // Ading 30px to the height given in the css
+// message.style.height =
+//   parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+/************* Cookie message ends ************/
 
 /******** STYLES, ATTRIBUTES AND CLASSES */
-
-// Setting background color and width of the cookie message
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
-
-// Ading 30px to the height given in the css
-message.style.height =
-  parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
 // document.documentElement.style.setProperty('--color-primary', 'orangered');
 
@@ -75,3 +84,128 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
       .scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+// tabs.forEach(tab => tab.addEventListener('click', () => console.log('TAB'))); This approach is not desirable
+
+//Using event delegation by attaching eventlistner to the parent element
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return; //if the clicked does not contain 'operations tab', then do nothing
+
+  // Remove active classes
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+
+  //Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
+/************* Menu fade animation starts ************/
+
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = opacity;
+      }
+      logo.style.opacity = opacity;
+    });
+  }
+};
+// nav.addEventListener('mouseover', function (e) {
+//   console.log(e.target);
+// });
+nav.addEventListener('mouseover', e => {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener('mouseout', e => {
+  handleHover(e, 1);
+});
+
+/************* Menu fade animation ends ************/
+
+/************* Sticky navigation  starts ************/
+
+// const initialCoords = section1.getBoundingClientRect();
+
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > initialCoords.top) {
+//     nav.classList.add('sticky');
+//   } else nav.classList.remove('sticky');
+// });
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    nav.classList.add('sticky');
+  } else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
+/************* Sticky navigation  ends ************/
+
+/************* Reveal section starts ************/
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    return;
+  }
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+//hides the sections
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+/************* Reveal section ends ************/
+
+/************* Lazy loading images starts ************/
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+/************* Lazy loading images end ************/
